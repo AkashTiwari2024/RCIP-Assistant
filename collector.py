77c8701bbcb1a647f0b10b749c18f7edccf70abc
AkashTiwari2024 <akash.tiwari90@yahoo.com>
2026-05-25 16:57:08 -0400

    Save

--- collector.py
+++ collector.py
@@ -1,8 +1,48 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-url = "https://www.jobbank.gc.ca/home"
+url = "https://www.jobbank.gc.ca/jobsearch/jobsearch?searchstring=information+technology+support"
 
 response = requests.get(url)
 
-print("Status Code:", response.status_code)
-print(response.text[:500])
+print("Status:", response.status_code)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+titles = soup.find_all("span", class_="noctitle")
+companies = soup.find_all("li", class_="business")
+locations = soup.find_all("li", class_="location")
+
+jobs = []
+
+for i in range(len(titles)):
+
+    title = titles[i].text.strip()
+
+    company = companies[i].text.strip() if i < len(companies) else "N/A"
+
+    location = (
+        locations[i]
+        .text
+        .replace("Location", "")
+        .strip()
+        if i < len(locations)
+        else "N/A"
+    )
+
+    job = {
+        "title": title,
+        "company": company,
+        "location": location
+    }
+
+    jobs.append(job)
+
+print(jobs)
+
+df = pd.DataFrame(jobs)
+
+df.to_csv("jobs.csv", index=False)
+
+print("jobs.csv saved successfully")
